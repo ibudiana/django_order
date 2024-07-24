a51d8c9010d6d53802cdb5c5503de90d14b96019
I Wayan Budiana <ibudiana@student.ciputra.ac.id>
2024-07-24 22:29:05 +0800

    commit ketiga penambahan login

--- kanhamedia/kanhamedia/urls.py
+++ kanhamedia/kanhamedia/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('order.urls'))
+    path('order/', include('order.urls')),
+    path('login/', include('login.urls'))
 ]
--- kanhamedia/order/urls.py
+++ kanhamedia/order/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('order/', views.order, name='order')
+    path('', views.order, name='order')
 ]
